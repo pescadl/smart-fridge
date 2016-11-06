bbeb77d1cf1a93e34192c65b04dec998b372db52
personifire <eric.wang.j13@Gmail.com>
2016-11-06 14:41:51 -0600

    update visual_recognition_v3.py into a fucntion

--- code/visual_recognition_v3.py
+++ code/visual_recognition_v3.py
@@ -9,34 +9,36 @@ import time
 
 THRESHOLD = .50
 
-x = mraa.Gpio(26)
-x.dir(mraa.DIR_OUT)
-x.write(1)
-call(['./pic'])
-x.write(0)
-outFile = open('Output.txt', 'w')
-visual_recognition = VisualRecognitionV3('2016-05-20', api_key='cf40138eb6764ea300b5fafe571e3369bc4f6ce9')
-
-output = {}
-with open(join(dirname(__file__), 'cpp-headless-output-COLOR.png'), 'rb') as image_file:
-    output = visual_recognition.classify(images_file=image_file)
-    outFile.write(json.dumps(output, indent=2))
-    outFile.close()
-
-
-
-inp = output.get('images')
-inp = inp[0].get('classifiers')
-
-out = []
-
-for group in inp:
-    classes = []
-    classes = group.get('classes')
-    for desc in classes:
-        if desc.get('score') >= THRESHOLD:
-            out.append(desc.get('class'))
-
-print out
-outfile = open('thingy.txt', 'w')
-outfile.write('\n'.join(out))
+def fridgescanner():
+    x = mraa.Gpio(26)
+    x.dir(mraa.DIR_OUT)
+    x.write(255)
+    #time.sleep(2)
+    call(['./pic'])
+    x.write(0)
+    outFile = open('Output.txt', 'w')
+    visual_recognition = VisualRecognitionV3('2016-05-20', api_key='cf40138eb6764ea300b5fafe571e3369bc4f6ce9')
+    
+    output = {}
+    with open(join(dirname(__file__), 'cpp-headless-output-COLOR.png'), 'rb') as image_file:
+        output = visual_recognition.classify(images_file=image_file)
+        outFile.write(json.dumps(output, indent=2))
+        outFile.close()
+    
+    
+    
+    inp = output.get('images')
+    inp = inp[0].get('classifiers')
+    
+    out = []
+    
+    for group in inp:
+        classes = []
+        classes = group.get('classes')
+        for desc in classes:
+            if desc.get('score') >= THRESHOLD:
+                out.append(desc.get('class'))
+    
+    print out
+    outfile = open('thingy.txt', 'w')
+    outfile.write('\n'.join(out))
