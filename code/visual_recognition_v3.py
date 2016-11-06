be1e3c5f78f4b0384adfd06a66f570b3719258d6
personifire <eric.wang.j13@Gmail.com>
2016-11-06 11:56:06 -0600

    sorta combined parse.py into visual_recognition_v3.py. hasn't crashed yet :D

--- code/visual_recognition_v3.py
+++ code/visual_recognition_v3.py
@@ -7,13 +7,36 @@ from subprocess import call
 import mraa
 import time
 
-#x = mraa.Gpio(103)
-#x.dir(mraa.DIR_OUT)
-#x.write(1)
-call(["./pic"])
-#x.write(0)
+THRESHOLD = .50
+
+x = mraa.Gpio(26)
+x.dir(mraa.DIR_OUT)
+x.write(1)
+call(['./pic'])
+x.write(0)
 outFile = open('Output.txt', 'w')
 visual_recognition = VisualRecognitionV3('2016-05-20', api_key='cf40138eb6764ea300b5fafe571e3369bc4f6ce9')
+
+output = {}
 with open(join(dirname(__file__), 'cpp-headless-output-COLOR.png'), 'rb') as image_file:
-	outFile.write(json.dumps(visual_recognition.classify(images_file=image_file), indent=2))
-	outFile.close()
+    output = visual_recognition.classify(images_file=image_file)
+    outFile.write(json.dumps(output, indent=2))
+    outFile.close()
+
+
+
+inp = output.get('images')
+inp = inp[0].get('classifiers')
+
+out = []
+
+for group in inp:
+    classes = []
+    classes = group.get('classes')
+    for desc in classes:
+        if desc.get('score') >= THRESHOLD:
+            out.append(desc.get('class'))
+
+print out
+outfile = open('thingy.txt', 'w')
+outfile.write('\n'.join(out))
